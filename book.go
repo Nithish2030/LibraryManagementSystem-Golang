b09f99626b2539b95ef88a5e0dc8a76cdb506d7b
Nithish Rajkumar <137717532+Nithish2030@users.noreply.github.com>
2025-01-24 18:54:38 +0530

    Update book.go

--- book.go
+++ book.go
@@ -1,26 +1,20 @@
-package main
+package library
 
-import "fmt"
-
-// Book struct
 type Book struct {
-	Title    string
-	Author   string
-	ISBN     string
-	Available bool
+	Title   string
+	Author  string
+	ISBN    string
+	IsEbook bool
 }
 
-// Method to initialize a new book
-func NewBook(title, author, isbn string, available bool) Book {
-	return Book{
-		Title:    title,
-		Author:   author,
-		ISBN:     isbn,
-		Available: available,
-	}
+type EBook struct {
+	Book
+	FileSize int
 }
 
-// Method to display book details
-func (b Book) DisplayDetails() string {
-	return fmt.Sprintf("Title: %s\nAuthor: %s\nISBN: %s\nAvailable: %t\n", b.Title, b.Author, b.ISBN, b.Available)
+type BookOperation interface {
+	AddBook(book Book) error
+	RemoveBook(isbn string) error
+	SearchBookByTitle(title string) *Book
+	ListBooks()
 }
